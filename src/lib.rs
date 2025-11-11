f19b57c319bfbe8b12b2ec99d884f3bdf92e23b2
Tony Kan <tianchengkan@gmail.com>
2025-11-10 23:33:06 -0800

    feat(lib): Add find_resonance_systems function to locate resonance systems

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ mod molecule;
 mod perception;
 mod resonance;
 
+pub use crate::find_resonance_systems_impl::find_resonance_systems;
+
 pub use core::atom::{AtomId, Element};
 pub use core::bond::{BondId, BondOrder};
 
@@ -14,3 +16,19 @@ pub use resonance::ResonanceSystem;
 pub use molecule::{Molecule, MoleculeBuildError};
 
 pub use crate::graph::traits;
+
+mod find_resonance_systems_impl {
+    use super::*;
+    use crate::graph::traits::MoleculeGraph;
+    use crate::perception::ChemicalPerception;
+
+    pub fn find_resonance_systems<G: MoleculeGraph>(
+        graph: &G,
+    ) -> Result<Vec<ResonanceSystem>, PerceptionError> {
+        let perception = ChemicalPerception::from_graph(graph)?;
+
+        let systems = resonance::find_systems(&perception);
+
+        Ok(systems)
+    }
+}
